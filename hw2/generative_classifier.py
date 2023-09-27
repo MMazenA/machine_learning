8ba1805f00acd740f2a497c5c2aa5d0604fc8de0
MMazenA <jazenjirza@gmail.com>
2023-09-27 19:29:41 -0400

    method seperation

--- hw2/generative_classifier.py
+++ hw2/generative_classifier.py
@@ -17,23 +17,79 @@ class naive_bayes():
         self.training_set = self.dataset.drop(self.testing_set.index)
         self.possible_outputs = self.dataset[self.predicting_feature].unique()
 
-    def discrete_probabilites(self):
+    def discrete_probabilities(self):
         """Produce probability chart for each features count with respect to its actual output"""
         dict_training_set = dict(tuple(self.training_set.groupby(self.predicting_feature)))
         col_names = list(self.discrete_cols)
-        self.discrete_probabalities = dict.fromkeys(col_names)
+        discrete_probabalities = dict.fromkeys(col_names)
         for key in dict_training_set:
             feature_dict = {}
             for col in col_names:
                 feature_dict[col] = (dict_training_set[key][col].value_counts())
-            self.discrete_probabalities[key] = feature_dict.copy()
-        return self.discrete_probabalities[key]
+            discrete_probabalities[key] = feature_dict.copy()
+        return discrete_probabalities
+    
+    def continous_probabilities(self):
+        """Produce probability chart for each features count with respect to its actual output"""
+        dict_training_set = dict(tuple(self.training_set.groupby(self.predicting_feature)))
+        col_names = list(self.continues_cols)
+        continous_probabilities = dict.fromkeys(self.possible_outputs)
+        for key in continous_probabilities:
+            feature_dict = {}
+            for feature in col_names:
+                feature_mean = np.mean(dict_training_set[key][feature])
+                feature_variance = np.std(dict_training_set[key][feature])
+                feature_dict[feature] = [feature_mean,feature_variance]
+            continous_probabilities[key] = feature_dict.copy()
+        return continous_probabilities
+        
+    def occurance_probabilities(self):
+        value_counts = self.training_set.value_counts(self.predicting_feature)
+        return value_counts
+    
+    def probability_table(self):
+        cont = self.continous_probabilities()
+        disc = self.discrete_probabilities()
+        for key in cont:
+            cont[key].update(disc[key])
+        return cont
     
     def train(self):
         self.split_data()
+        self.probabilities = self.probability_table()
+        self.occurance_prob = self.occurance_probabilities()
+
+    def predict_single(self,row: pd.DataFrame):
+        sol = {} #dict of size class_count for all the chances of the test case
+        for key in self.possible_outputs:
+            # print(self.occurance_prob[key],end=" ")
+            likelihood = np.log(self.occurance_prob[key])
+            for feature in row.columns:
+                if feature in self.continues_cols:
+                    #z score
+                    mstd = self.probabilities[key][feature]
+                    mean = mstd[0]
+                    std = mstd[1]
+                    likelihood+=np.log((row[feature] - mean)/std)
+                    pass
+                else:
+                    row_feature = row[feature]
+                    feature_occurance = self.probabilities[key][feature][row_feature].iloc[0]
+                    print(feature_occurance,row_feature)
+                    feature_total = sum(self.probabilities[key][feature])
+                    likelihood+= np.log(feature_occurance/feature_total)
+                    
+                    
+                    
+            print(likelihood)
+            sol[key] = likelihood
+
+            
+    def test(self):
+
+        self.predict_single(self.testing_set.head(1))
 
 
-                
 
     def normalize(self,continues_cols):
         """Normalize columns that are continous using Z score."""
@@ -58,3 +114,10 @@ data.drop(["id"],axis=1,inplace=True)
 x = naive_bayes(data,"num")
 x.normalize(["age", "trestbps", "chol", "thalch", "oldpeak"])
 x.train()
+x.test()
+
+# df = (data[["age"]] - data[["age"]].mean())/data[["age"]].std() 
+
+# print(df.tail(5))
+# print(df[["age"]].mean())
+# print("Wjat",(53-data[["age"]].mean())/data[["age"]].std())
